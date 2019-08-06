8a3ba91dd5095c084fdff3ebca03e7ce5085a164
Teng Man <mantou0520@users.noreply.github.com>
2019-08-06 09:22:53 +0800

    Update column.cpp

--- column.cpp
+++ column.cpp
@@ -34,6 +34,7 @@ int main(int argc, char **argv) try
     size_t Nproc = 1;
     if (argc>=3) Nproc = atoi(argv[2]);
 
+    // set up the input file
     String filekey  (argv[1]);
     String filename (filekey+".inp");
     if (!Util::FileExists(filename)) throw new Fatal("File <%s> not found",filename.CStr());
@@ -109,7 +110,8 @@ int main(int argc, char **argv) try
     // domain
     DEM::Domain d;
 
-    //Add the granular column
+    //Add the granular column, it can be Voronoi packing, or spherical particle packings.
+    // We can modify this code to obtain other kind of particle shapes
     if (ptype=="voronoi") d.AddVoroPack(-1,R,Lx,Ly,Lz,Nx,Ny,Nz,rho,Cohesion/*no cohesion*/,true/*periodic construction for angularity*/,seed,fraction);
     else if (ptype=="sphereboxnormal") 
     {
@@ -127,7 +129,7 @@ int main(int argc, char **argv) try
 	
     //Determinaning the bounding box
     Vec3_t Xmin,Xmax;
-    d.BoundingBox(Xmin,Xmax);
+    d.BoundingBox(Xmin,Xmax); // it is better to obtain the bounding box again in the end to calculate the spreadness of the particles
 
     //Adding plate at the base of the column
     d.AddPlane(-2,Vec3_t(0.0,0.0,Xmin(2)-R),R,plane_x*Lz,plane_y*Lz,rho);
@@ -146,6 +148,7 @@ int main(int argc, char **argv) try
     }
 	
     // Change the shape of cross-section
+    // In this case, we are obtaining a cylindrical column, if CrossSection == "Circle"
     if (CrossSection=="Circle")
     {
 	for (size_t np=0;np<d.Particles.Size();np++)
